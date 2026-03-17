22927d739b703b9571f0e588e53a6a0539d38b08
thanongsaklaoketkich <thanong_777@hotmail.com>
2026-03-17 14:36:05 +0100

    Remove dist and fix prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Application } from "./components/app/application.js";
+import { Application } from "./components/app/application";
 
 createRoot(document.getElementById("app")!).render(<Application />);
